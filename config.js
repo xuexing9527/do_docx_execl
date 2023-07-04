bfd27f6dfd99a62c9629b30c9bb2308c7a3cc5c1
x <ts@ts.com>
2023-07-04 17:17:55 +0800

    拆分config

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    outputPath: './output',
+    docxsPath: './docxs',
+    reg: {
+        includeTargetReg: /<w:t>-=<\/w:t>.*?>(\d+?)<.*?<w:t>=-<\/w:t>/g,
+        targetReg: /<w:t>(\d+?)<\/w:t>/,
+    },
+
+}
+module.exports = config
