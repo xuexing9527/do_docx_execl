042c32e082072de0472709f6742dc1313c8d6c42
x <xuexing9527@163.com>
2024-01-16 12:37:10 +0800

    feat

--- src/readAndWrite/index.js
+++ src/readAndWrite/index.js
@@ -1,14 +1,7 @@
-// const DocReader = require('./../../reader/DocReader')
-
-// const docReader = new DocReader({ path: './data/docEngineeringCostClassTwo', outputPath: './data/docEngineeringCostClassTwo/output'})
-
-// docReader.read()
-
 const DocWriter = require('../../writer/DocWriter')
 
-const docxWriter = new DocWriter({ path: './data/docEngineeringCostClassTwo/062200230201常文丽实验报告合并版.docx' })
-
-console.log(docxWriter)
+// console.log(DocWriter)
+const docxWriter = new DocWriter({ path: './data/docEngineeringCostClassTwo/test.docx' })
 
 // docxWriter.insertContent('2024年1月15日00:25:53')
 // docxWriter.save()
